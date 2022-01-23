dad3fb268f8ddbbdcd852ec03f9374c109706e56
atul kumar <atulkumarit05@gmail.com>
2022-01-23 13:43:22 +0000

    login set cookie

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -134,6 +134,9 @@ $('.logbtn').on('click',function(e){
         email:logusername,
         password:logpassword
     }
+
+    eraseCookie('token');
+    eraseCookie('userdetails');
     
     // setup some local variables
 
@@ -155,10 +158,8 @@ $('.logbtn').on('click',function(e){
 
         console.log(response);
         if(response.status == "ok"){
-            $.removeCookie("token");
-            $.removeCookie("userdetails");
-            $.cookie("token",response.token);
-            $.cookie("userdetails", response.user);
+            setCookie("token",response.token,1)
+            setCookie("userdetails",response.user,1)
             window.location.href = "main.html"
 
         }
@@ -187,6 +188,29 @@ $('.logbtn').on('click',function(e){
     });
 });
 
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {   
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
 
 
 
